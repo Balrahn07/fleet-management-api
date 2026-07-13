fc8d1fa76e18ef3abdbaf3ffde2624e9a1f8d5ad
Balrahn07 <dnz.akbs@gmail.com>
2026-07-13 23:53:02 +0200

    feat: Configure TraceLayer

--- src/routes.rs
+++ src/routes.rs
@@ -7,14 +7,17 @@ use crate::{
 };
 use axum::{
     Router,
-    http::HeaderName,
+    http::{HeaderName, Request},
     routing::{delete, get, post, put},
 };
-
+use std::time::Duration;
 use tower_http::{
+    classify::ServerErrorsFailureClass,
     request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
     trace::TraceLayer,
 };
+
+use tracing::{Span, info_span};
 
 pub fn create_routes(state: AppState) -> Router {
     let request_id_header = HeaderName::from_static("x-request-id");
@@ -28,7 +31,42 @@ pub fn create_routes(state: AppState) -> Router {
         .route("/vehicles/{id}", delete(delete_vehicle))
         .route("/vehicles/{id}/assign-driver", post(assign_driver))
         .with_state(state)
-        .layer(TraceLayer::new_for_http())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let request_id = request
+                        .headers()
+                        .get("x-request-id")
+                        .and_then(|value| value.to_str().ok())
+                        .unwrap_or("unknown");
+
+                    info_span!(
+                        "http_request",
+                        request_id = %request_id,
+                        method = %request.method(),
+                        uri = %request.uri(),
+                        version = ?request.version(),
+                    )
+                })
+                .on_response(
+                    |response: &axum::response::Response, latency: Duration, _span: &Span| {
+                        tracing::info!(
+                            status = %response.status(),
+                            latency_ms = latency.as_millis(),
+                            "Request completed"
+                        );
+                    },
+                )
+                .on_failure(
+                    |failure: ServerErrorsFailureClass, latency: Duration, _span: &Span| {
+                        tracing::error!(
+                            failure = %failure,
+                            latency_ms = latency.as_millis(),
+                            "Request failed"
+                        );
+                    },
+                ),
+        )
         .layer(PropagateRequestIdLayer::new(request_id_header.clone()))
         .layer(SetRequestIdLayer::new(request_id_header, MakeRequestUuid))
 }
