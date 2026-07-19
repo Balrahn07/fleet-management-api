b9b9d84f81707bfa10bce442fe26a088d2ace203
Balrahn07 <dnz.akbs@gmail.com>
2026-07-19 15:16:18 +0200

    feat: Add in-memory cache

---
+++ src/cache.rs
@@ -0,0 +1,32 @@
+use async_trait::async_trait;
+use dashmap::DashMap;
+
+#[async_trait]
+pub trait Cache: Send + Sync {
+    async fn get(&self, key: &str) -> Option<String>;
+
+    async fn set(&self, key: &str, value: String);
+}
+
+pub struct InMemoryCache {
+    store: DashMap<String, String>,
+}
+
+impl InMemoryCache {
+    pub fn new() -> Self {
+        Self {
+            store: DashMap::new(),
+        }
+    }
+}
+
+#[async_trait]
+impl Cache for InMemoryCache {
+    async fn get(&self, key: &str) -> Option<String> {
+        self.store.get(key).map(|value| value.clone())
+    }
+
+    async fn set(&self, key: &str, value: String) {
+        self.store.insert(key.to_owned(), value);
+    }
+}
